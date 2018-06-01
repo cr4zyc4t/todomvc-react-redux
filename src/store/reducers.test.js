e4c5a4b71caece5f72e2d975bd9fc1d1c1c97925
Vu Quoc. Toan <toanvq@toshiba-tsdv.com>
2018-06-01 16:02:58 +0700

    update test

--- src/store/reducers.test.js
+++ src/store/reducers.test.js
@@ -1,30 +1,30 @@
 import reducers from './reducers';
 
-test('reducers toggle all', () => {
+test('toggle all', () => {
   let state;
   state = reducers({ tasks: [{ id: 'cc762288-7ef0-4609-996f-43dfaa624f7a', title: '232', completed: false }, { id: '5d97ec34-0ed8-486f-b46f-816549f6d73c', title: 'wd23', completed: false }, { id: 'e2888eb8-e037-40e6-8ca7-b269787baba3', title: 'wdwd', completed: true }, { id: '3b817505-39d5-4d31-91ae-1c1329530ba2', title: 'qwdwqd', completed: false }], filter: 'all' }, { type: 'TOGGLE_ALL', payload: { checked: true } });
   expect(state).toEqual({ tasks: [{ id: 'cc762288-7ef0-4609-996f-43dfaa624f7a', title: '232', completed: true }, { id: '5d97ec34-0ed8-486f-b46f-816549f6d73c', title: 'wd23', completed: true }, { id: 'e2888eb8-e037-40e6-8ca7-b269787baba3', title: 'wdwd', completed: true }, { id: '3b817505-39d5-4d31-91ae-1c1329530ba2', title: 'qwdwqd', completed: true }], filter: 'all' });
 });
 
-test('reducers', () => {
+test('clear completed tasks', () => {
   let state;
   state = reducers({ tasks: [{ id: 'cc762288-7ef0-4609-996f-43dfaa624f7a', title: '232', completed: true }, { id: '5d97ec34-0ed8-486f-b46f-816549f6d73c', title: 'wd23', completed: true }, { id: 'e2888eb8-e037-40e6-8ca7-b269787baba3', title: 'wdwd', completed: true }, { id: '3b817505-39d5-4d31-91ae-1c1329530ba2', title: 'qwdwqd', completed: true }], filter: 'all' }, { type: 'CLEAR_COMPLETED' });
   expect(state).toEqual({ tasks: [], filter: 'all' });
 });
 
-test('reducers', () => {
+test('add task 1', () => {
   let state;
   state = reducers({ tasks: [], filter: 'all' }, { type: 'ADD_TASK', payload: { id: '7dc4e625-d336-40d3-92f6-b73d12d84670', title: '1' } });
   expect(state).toEqual({ tasks: [{ id: '7dc4e625-d336-40d3-92f6-b73d12d84670', title: '1', completed: false }], filter: 'all' });
 });
 
-test('reducers', () => {
+test('add task 2', () => {
   let state;
   state = reducers({ tasks: [{ id: '7dc4e625-d336-40d3-92f6-b73d12d84670', title: '1', completed: false }], filter: 'all' }, { type: 'ADD_TASK', payload: { id: 'ca14dc02-9129-44e6-a3fa-f0877c8e2713', title: '2' } });
   expect(state).toEqual({ tasks: [{ id: 'ca14dc02-9129-44e6-a3fa-f0877c8e2713', title: '2', completed: false }, { id: '7dc4e625-d336-40d3-92f6-b73d12d84670', title: '1', completed: false }], filter: 'all' });
 });
 
-test('reducers', () => {
+test('add task 3', () => {
   let state;
   state = reducers({ tasks: [{ id: 'ca14dc02-9129-44e6-a3fa-f0877c8e2713', title: '2', completed: false }, { id: '7dc4e625-d336-40d3-92f6-b73d12d84670', title: '1', completed: false }], filter: 'all' }, { type: 'ADD_TASK', payload: { id: 'e1270010-1770-4ba8-9924-910c34123a55', title: '3' } });
   expect(state).toEqual({ tasks: [{ id: 'e1270010-1770-4ba8-9924-910c34123a55', title: '3', completed: false }, { id: 'ca14dc02-9129-44e6-a3fa-f0877c8e2713', title: '2', completed: false }, { id: '7dc4e625-d336-40d3-92f6-b73d12d84670', title: '1', completed: false }], filter: 'all' });
